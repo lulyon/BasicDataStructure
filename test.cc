640e47e388c0a131feaa6278b1e3a79d36dffcba
Lu Liang <lulyonde@gmail.com>
2013-05-29 21:22:15 +0800

    Create test.cc

---
+++ test.cc
@@ -0,0 +1,15 @@
+/*
+ * test.cc
+ *
+ *  Created on: 2013-5-28
+ *      Author: luliang
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("hello world!\n");
+	return 0;
+}
