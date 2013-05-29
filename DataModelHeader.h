4b53a9b55c40af5b8f80c43deb586d34f3134983
Lu Liang <lulyonde@gmail.com>
2013-05-29 21:13:03 +0800

    Create DataModelHeader.h

---
+++ DataModelHeader.h
@@ -0,0 +1,16 @@
+/*
+ * DataModelHeader.h
+ *
+ *  Created on: 2013-5-29
+ *      Author: luliang
+ */
+
+#ifndef DATAMODELHEADER_H_
+#define DATAMODELHEADER_H_
+
+class KeyValue;
+// to support more kind of DataModel.
+typedef KeyValue DataModel;
+
+#endif /* DATAMODELHEADER_H_ */
+
